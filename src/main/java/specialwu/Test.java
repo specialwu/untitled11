359baaf8fd5bcbbeec0b4dd86eb42c6072a59930
specialwu <73089916+specialwu@users.noreply.github.com>
2020-10-22 14:54:47 +0800

    吴琪测试

---
+++ src/main/java/specialwu/Test.java
@@ -0,0 +1,13 @@
+package specialwu;
+
+/**
+ * @Author:wuqi
+ * @DATE: 2020/10/22 0022
+ * @TIME: 14:53
+ * @PROJECT_NAME: untitled11
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
